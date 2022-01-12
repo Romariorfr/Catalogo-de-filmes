cd556ce9bf4974b4c08e0709e15a8070af2bb14e
Romário Ferreira <romarioferreirarezende@hotmail.com>
2022-01-12 06:23:32 -0300

    MovieCard

---
+++ frontend/src/components/MovieScore/index.tsx
@@ -0,0 +1,19 @@
+import MovieStars from "components/MovieStars";
+import './styles.css'
+
+function MovieScored() {
+
+    const score = 3.5;
+    const count = 3;
+  return (
+    <div className="dsmovie-score-container">
+      <p className="dsmovie-score-value">
+        {score > 0 ? score.toFixed(1) : "-"}
+      </p>
+      <MovieStars />
+      <p className="dsmovie-score-count">{count} avaliações</p>
+    </div>
+  );
+}
+
+export default MovieScored;
