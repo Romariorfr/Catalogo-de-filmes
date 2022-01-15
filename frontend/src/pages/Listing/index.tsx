24343a907886be645f33bbdbde0f1d146a106151
Romário Ferreira <romarioferreirarezende@hotmail.com>
2022-01-14 22:35:19 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -8,41 +8,47 @@ import { BASE_URL } from "utils/requests";
 function Listing() {
   const [pageNumber, setPageNumber] = useState(0);
 
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true,
+  });
+
   useEffect(() => {
-   
-    axios.get(`${BASE_URL}/movies?size=12&page=1`).then((response) => {
-      const data = response.data as MoviePage;
-      console.log(data)
-      setPageNumber(data.number);
-    });
-  }, []);
+    axios
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+      .then((response) => {
+        const data = response.data as MoviePage;
+        setPage(data);
+      });
+  }, [pageNumber]);
+
+  const movie = {
+    id: 1,
+    image:
+      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    title: "The Witcher",
+    count: 2,
+    score: 4.5,
+  };
 
   return (
     <>
       <Pagination />
-      <p>{pageNumber}</p>
 
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
+          {page.content.map((movie) => (
+            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard movie={movie} />
+            </div>
+          ))}
         </div>
       </div>
     </>
